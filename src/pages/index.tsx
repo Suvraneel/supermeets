f860315ffca4547146dab648a615604bf88d033e
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-10-15 01:06:56 +0530

    loader & ui tweaks :feelsgood:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,24 @@ import { swera } from "@fonts"
 import Image from "next/image"
 import { cardsContent } from "@api/cardContent"
 import Spliner from "@components/Spliner"
+import { ArrowRightIcon } from "@heroicons/react/24/outline"
 
 export default function Home() {
   return (
     <main>
       <Spliner scene={"https://prod.spline.design/TmLa-bcpmxHw90XL/scene.splinecode"}/>
-      <div className="flex-1 w-full h-full p-4 lg:px-40 overflow-visible flex flex-col justify-start items-start gap-8 pt-40">
+      <div className="flex-1 w-full h-full p-4 lg:px-40 overflow-visible flex flex-col justify-start items-start gap-16 pt-40">
 
       <div className="relative flex place-items-center before:absolute before:h-[200px] before:w-[780px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/2 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[260px] z-[-1]">
-        <h1 className={`text-6xl text-white ${swera.className}`}>NFT<br />Omegle</h1>
+        <h1 className={`text-6xl text-white ${swera.className}`}>SuperMeet</h1>
       </div>
+      <button
+            type="button"
+            className="flex w-40 items-center justify-center rounded-md py-3 text-slate-100 font-semibold bg-blue-600 group hover:bg-blue-900"
+          >
+            Start Meeting
+            <ArrowRightIcon className="ml-2 h-4 w-4 group-hover:translate-x-2 transition-transform" />
+          </button>
       {/* <div className="w-full h-full flex flex-col gap-4">
         <div className="w-full h-full flex flex-row flex-wrap md:flex-nowrap gap-8 justify-between">
           <Card content={cardsContent[0]} />
--- src/pages/itsamatch/index.tsx
+++ src/pages/itsamatch/index.tsx
@@ -11,20 +11,21 @@ const ItsAMatch = () => {
           <Realistic />
           <h1 className='text-white text-3xl font-bold'>It&apos;s a Match!</h1>
           <div className="flex justify-evenly items-center w-full h-full relative">
-            <div className="w-1/3 h-96 relative">
+            <div className="w-1/3 aspect-square relative">
               <Image
                 src="/guy.jpg"
                 alt="itsamatch"
                 fill
-                className={`rounded-full ${styles.animate_left_to_center} bg-contain`}              />
+                className={`rounded-full ${styles.animate_left_to_center} bg-cover`}
+                />
             </div>
             <h1 className={`text-8xl font-thin ${styles.stamp}`}>X</h1>
-            <div className="w-1/3 h-96 relative">
+            <div className="w-1/3 aspect-square relative">
               <Image
                 src="/gril.jpg"
                 alt="itsamatch"
                 fill
-                className={`rounded-full ${styles.animate_right_to_center} bg-contain`}
+                className={`rounded-full ${styles.animate_right_to_center} bg-cover`}
               />
             </div>
           </div>
